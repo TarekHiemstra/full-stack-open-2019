ea3f6fbf8407a6bcad208ff8ddcba8c98b4485a8
Tarek Hiemstra <tarek.hiemstra@metropolia.fi>
2019-11-22 14:15:17 +0200

    Add 3.5: Phonebook backend step5

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -28,6 +28,33 @@ let persons = [
   }
 ]
 
+// Id is generated in a range between 1 and 10 billion
+const generateId = () => {
+  const max = 10000000000
+  const min = 1000000000
+  return(Math.floor((Math.random() * (max - min) + min)))
+}
+
+app.post('/api/persons', (request, response) => {
+  const body = request.body
+
+  if (!body.name) {
+    return response.status(400).json({
+      error: 'person missing'
+    })
+  }
+
+  const person = {
+    name: body.name,
+    number: body.number,
+    id: generateId(),
+  }
+
+  persons = persons.concat(person)
+  response.json(person)
+})
+
+
 app.get('/info', (req, res) => {
   res.send(`<p>Phonebook has info for ${persons.length} people</p>
             <p>${new Date()}</p>`)
