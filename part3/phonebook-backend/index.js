5b5686813b6d6fd49786d999f2155699c2434033
Tarek Hiemstra <tarek.hiemstra@metropolia.fi>
2019-11-22 11:33:14 +0200

    Add 3.3: Phonebook backend step3

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -37,6 +37,16 @@ app.get('/api/persons', (request, response) => {
   response.json(persons)
 })
 
+app.get('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  const person = persons.find(person => person.id === id)
+  if (person) {
+    response.json(person)
+  } else {
+    response.status(404).end()
+  }
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
