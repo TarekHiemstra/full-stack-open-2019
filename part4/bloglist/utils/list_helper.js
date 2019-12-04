5cf162c0ec74ea56a73af8a075a113b792ba20dc
Tarek Hiemstra <tarek.hiemstra@metropolia.fi>
2019-12-04 14:41:33 +0200

    Add 4.4: helper functions and unit tests, step2

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -2,6 +2,11 @@ const dummy = (blogs) => {
   return 1
 }
 
+const totalLikes = (blogs) => {
+  return blogs.reduce((accu, curr) => accu + curr.likes, 0) || 0
+}
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes
 }
