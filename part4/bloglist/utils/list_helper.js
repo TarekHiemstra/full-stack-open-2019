086111d69fb7fa09354c504450a4ec2690564a98
Tarek Hiemstra <tarek.hiemstra@metropolia.fi>
2019-12-04 18:14:31 +0200

    Add 4.5*: helper functions and unit tests, step3

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -6,7 +6,15 @@ const totalLikes = (blogs) => {
   return blogs.reduce((accu, curr) => accu + curr.likes, 0) || 0
 }
 
+const favoriteBlog = (blogs) => {
+  const topBlog = blogs.reduce((a, c) => c.likes > a.likes ? c : a)
+  // Omit the the 3 properties '_id', 'url' and '__v'
+  const { _id, url, __v, ...topBlog3 } = topBlog
+  return topBlog3
+}
+
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog
 }
