01e9379a68b204fc5154d2732153092644436b9c
Tarek Hiemstra <tarek.hiemstra@metropolia.fi>
2019-12-07 10:59:59 +0200

    Add 4.9*: Blog list tests, step2
    Add some helper functions in tests/test_helper.js
    Now the previous tests use the initialBlogs of the test_helper.js file.
    
    I already implemented the toJSON method when I created the model.

--- part4/bloglist/tests/bloglist_api.test.js
+++ part4/bloglist/tests/bloglist_api.test.js
@@ -1,10 +1,20 @@
-const mongoose = require('mongoose')
 const supertest = require('supertest')
+const mongoose = require('mongoose')
+const helper = require('./test_helper')
 const app = require('../app')
-
 const api = supertest(app)
 
-describe('correct amount of blog posts in the JSON format', () => {
+const Blog = require('../models/blog')
+
+beforeEach(async () => {
+  await Blog.deleteMany({})
+  const blogObjects = helper.initialBlogs
+    .map(blog => new Blog(blog))
+  const promiseArray = blogObjects.map(blog => blog.save())
+  await Promise.all(promiseArray)
+})
+
+describe('get requests', () => {
   test('blogs are returned as json', async () => {
     await api
       .get('/api/blogs')
@@ -12,12 +22,16 @@ describe('correct amount of blog posts in the JSON format', () => {
       .expect('Content-Type', /application\/json/)
   })
 
-  test('there are two blogs', async () => {
+  test('there are five blogs', async () => {
     const response = await api.get('/api/blogs')
-    expect(response.body.length).toBe(2)
+    expect(response.body.length).toBe(helper.initialBlogs.length)
+  })
+
+  test('the unique identifier property of the blog posts is named id', async () => {
+    const response = await api.get('/api/blogs')
+    expect(response.body[0].id).toBeDefined()
   })
 })
-
 
 afterAll(() => {
   mongoose.connection.close()
