929788cad66e3594dbf68cdecaa53b0d06abe9b5
Tarek Hiemstra <tarek.hiemstra@metropolia.fi>
2019-12-09 21:07:50 +0200

    Add 4.18: bloglist expansion, step7

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -5,6 +5,7 @@ const app = express()
 const cors = require('cors')
 const usersRouter = require('./controllers/users')
 const blogsRouter = require('./controllers/blogs')
+const loginRouter = require('./controllers/login')
 const middleware = require('./utils/middleware')
 const mongoose = require('mongoose')
 
@@ -27,6 +28,7 @@ app.use(express.static('build'))
 app.use(bodyParser.json())
 app.use(middleware.requestLogger)
 
+app.use('/api/login', loginRouter)
 app.use('/api/users', usersRouter)
 app.use('/api/blogs', blogsRouter)
 
