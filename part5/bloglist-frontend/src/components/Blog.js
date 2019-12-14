35e9bb52604a3211dda772318c63dc6c1eb507a1
Tarek Hiemstra <tarek.hiemstra@metropolia.fi>
2019-12-14 21:37:08 +0200

    Add 5.6* Blog list frontend, step6

--- part5/bloglist-frontend/src/components/Blog.js
+++ part5/bloglist-frontend/src/components/Blog.js
@@ -1,8 +1,23 @@
-import React from 'react'
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-)
+import React, {useState} from 'react'
+
+const Blog = ({ blog }) => {
+  const [expanded, setExpanded] = useState(false)
+
+  const hideWhenExpanded = { display: expanded ? 'none' : '' }
+  const showWhenExpaned = { display: expanded ? '' : 'none' }
+
+  return (
+    <div className='blog'>
+      <div style={hideWhenExpanded} onClick={() => setExpanded(!expanded)}>
+        {blog.title} {blog.author}
+      </div>
+      <div style={showWhenExpaned} onClick={() => setExpanded(!expanded)}>
+        {blog.title} {blog.author} <br />
+        <a href={blog.url}>{blog.url}</a> <br />
+        {blog.likes} likes <button>like</button><br />
+        added by {blog.user.name} <br />
+      </div>
+    </div>
+)}
 
 export default Blog
