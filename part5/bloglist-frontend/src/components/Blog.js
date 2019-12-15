5f5069a6ff8b23b7627c8fd141285bc9188bae84
Tarek Hiemstra <tarek.hiemstra@metropolia.fi>
2019-12-15 20:26:53 +0200

    Add 5.10*: Blog list frontend, step10

--- part5/bloglist-frontend/src/components/Blog.js
+++ part5/bloglist-frontend/src/components/Blog.js
@@ -2,22 +2,26 @@ import React, {useState} from 'react'
 
 const Blog = ({ blog, user, handleLikes, handleRemove }) => {
   const [expanded, setExpanded] = useState(false)
-
   const hideWhenExpanded = { display: expanded ? 'none' : '' }
-  const showWhenExpaned = { display: expanded ? '' : 'none' }
+  const showWhenExpanded = { display: expanded ? '' : 'none' }
+
   const id = blog.id
+  const currentUser = JSON.parse(window.localStorage.getItem('loggedBlogappUser'))
 
   return (
     <div className='blog'>
       <div style={hideWhenExpanded} onClick={() => setExpanded(!expanded)}>
         {blog.title} {blog.author}
       </div>
-      <div style={showWhenExpaned} onClick={() => setExpanded(!expanded)}>
+      <div style={showWhenExpanded} onClick={() => setExpanded(!expanded)}>
         {blog.title} {blog.author} <br />
         <a href={blog.url}>{blog.url}</a> <br />
         {blog.likes} likes <button type='button' value={id} onClick={handleLikes}>like</button> <br />
         added by {user.name} <br />
-        <button type='button' value={id} onClick={handleRemove}>remove</button> <br />
+        {
+        currentUser.username === user.username &&
+        <button type='button' value={id} onClick={handleRemove}>remove</button> 
+        }
       </div>
     </div>
 )}
