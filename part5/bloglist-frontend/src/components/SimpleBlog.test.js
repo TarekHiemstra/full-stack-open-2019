041e0c3cc2dedb145ee22254e1d81534006c8353
Tarek Hiemstra <tarek.hiemstra@metropolia.fi>
2019-12-19 22:48:12 +0100

    Add 5.14: Blog list tests, step2

--- part5/bloglist-frontend/src/components/SimpleBlog.test.js
+++ part5/bloglist-frontend/src/components/SimpleBlog.test.js
@@ -1,14 +1,15 @@
 import React from 'react'
-import { render } from '@testing-library/react'
+import { render, fireEvent } from '@testing-library/react'
 import SimpleBlog from './SimpleBlog'
 
 describe('<SimpleBlog />', () => {
   let component
+  let mockHandler
   const testBlog = { title: 'Test title', author: 'Test author', likes: 100 }
-  const handleClick = () =>  console.log ('click!')
 
   beforeEach(() => {
-    component = render( <SimpleBlog blog={testBlog} onclick={handleClick} /> )
+    mockHandler = jest.fn()
+    component = render( <SimpleBlog blog={testBlog} onClick={mockHandler} /> )
   })
 
   test('renders the title', () => {
@@ -24,5 +25,12 @@ describe('<SimpleBlog />', () => {
   test('renders the likes', () => {
     const div = component.container.querySelector('.likes')
     expect(div).toHaveTextContent('blog has 100 likes')
+  })
+
+  test('clicking the button twice calls event handler twice', () => {
+    const button = component.getByText('like')
+    fireEvent.click(button)
+    fireEvent.click(button)
+    expect(mockHandler.mock.calls.length).toBe(2)
   })
 })
